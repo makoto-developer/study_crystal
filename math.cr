dac42fdbdeb3acf93adb4da10b52c771a0092ebb
makoto <72484465+makoto-engineer@users.noreply.github.com>
2021-09-17 23:54:48 +0900

    math

---
+++ math.cr
@@ -0,0 +1,8 @@
+p! Math.cos(1),     # cosine
+  Math.sin(1),      # sine
+  Math.tan(1),      # tangent
+  Math.log(42),     # natural logarithm
+  Math.log10(312),  # logarithm to base 10
+  Math.log(312, 5), # logarithm to base 5
+  Math.sqrt(9)      # square root
+
